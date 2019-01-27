1afb754c38455bf93590392533857eccb2483f75
clindv <clindv@protonmail.com>
2019-01-27 20:01:43 +0800

    dft

--- src/fourier_transform/core.clj
+++ src/fourier_transform/core.clj
@@ -1,7 +1,11 @@
 (ns fourier-transform.core
   (:gen-class))
-
+(defn dft [v]
+  (let [n (count v)]
+    (for [i (range n)]
+      (Math/hypot
+       (apply + (map * v (map (comp #(Math/cos %) (partial * 2 i Math/PI (/ n))) (range n))))
+       (apply + (map * v (map (comp #(Math/sin %) (partial * 2 i Math/PI (/ n))) (range n))))))))
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
--- test/fourier_transform/core_test.clj
+++ test/fourier_transform/core_test.clj
@@ -1,7 +1,6 @@
 (ns fourier-transform.core-test
   (:require [clojure.test :refer :all]
             [fourier-transform.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest tranform
+  (testing "dft"
+    (is (= (apply + (dft [1 2 3 4])) 17.65685424949238))))
