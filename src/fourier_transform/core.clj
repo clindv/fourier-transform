91c42973b2b5c28e5b1fdc66444a3227dafc11f9
clindv <clindv@protonmail.com>
2019-01-27 22:53:41 +0800

    dft lazy & memoize & speed test

--- src/fourier_transform/core.clj
+++ src/fourier_transform/core.clj
@@ -6,6 +6,24 @@
       (Math/hypot
        (apply + (map * v (map (comp #(Math/cos %) (partial * 2 i Math/PI (/ n))) (range n))))
        (apply + (map * v (map (comp #(Math/sin %) (partial * 2 i Math/PI (/ n))) (range n))))))))
+(defn dft-lazy [v]
+  (let [n (count v)]
+    (for [i (range n)]
+      (#(Math/hypot (first %) (second %))
+       (apply (partial map +)
+              (map #(map (partial * %1) %2) v
+                   (map (juxt (fn [x] (Math/cos x)) (fn [x] (Math/sin x)))
+                        (map #(* 2 i Math/PI (/ n) %)
+                             (range n)))))))))
+(defn dft-lazy-memoize [v]
+  (let [n (count v)]
+    (for [i (range n)]
+      (#(Math/hypot (first %) (second %))
+       (apply (partial map +)
+              (map #(map (partial * %1) %2) v
+                   (map (juxt (memoize (fn [x] (Math/cos x))) (memoize (fn [x] (Math/sin x))))
+                        (map #(* 2 i Math/PI (/ n) %)
+                             (range n)))))))))
 (defn -main
   [& args]
   (println "Hello, World!"))
--- test/fourier_transform/core_test.clj
+++ test/fourier_transform/core_test.clj
@@ -3,4 +3,12 @@
             [fourier-transform.core :refer :all]))
 (deftest tranform
   (testing "dft"
-    (is (= (apply + (dft [1 2 3 4])) 17.65685424949238))))
+    (is (= (apply + (dft [1 2 3 4])) 17.65685424949238)))
+  (testing "dft-lazy"
+    (is (= (apply + (dft-lazy [1 2 3 4])) 17.65685424949238))
+    (pr "time of dft 512: ")
+    (time (nth (dft (range 512)) 500))
+    (pr "time of dft-lazy 512: ")
+    (time (nth (dft-lazy (range 512)) 500))
+    (pr "time of dft-lazy-memoize 512: ")
+    (time (nth (dft-lazy-memoize (range 512)) 500))))
